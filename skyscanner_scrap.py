273efaf46d6116fe69c4ad2e508fb436b96a0b31
qnaudan <nitneuqsocial@outlook.com>
2020-05-08 15:29:26 +0200

    requirements

--- skyscanner_scrap.py
+++ skyscanner_scrap.py
@@ -18,15 +18,10 @@ N_processes = 3
 # Number of web browsers opened at same time (depends empirically of internet speed)
 N_browsers = 3
 
-# Our Departure and Arrival Cities / One-Way or Return flights
-From = input("Departure City : ")
-To = input("Destination City : ")
-Return = int( \
-         input("Estimated stay in number of days (0 for One-Way flight) : "))
 
 # In how many days and for which range to look up flight fares
 # Default : starting next month and for 3 months
-Start, Iterations = 30, 50
+Start, Iterations = 30, 7
 
 
 # URL body for this situation : 1 adult, French website, EUR currency
@@ -61,8 +56,10 @@ def create_trips(From, To, Return, Start, Iterations):
     browser.find_element_by_id("fsc-origin-search").send_keys(From)
     browser.find_element_by_id("fsc-destination-search").send_keys(To)
     browser.find_element_by_id("fsc-trip-type-selector-one-way").click()
+    browser.find_element_by_id("fsc-origin-search").click()
+    browser.find_element_by_id("fsc-destination-search").click()
     browser.find_element_by_xpath('//button[contains(text(), "Trouver un vol")]').click()
-    sleep(0.5)
+    sleep(2.5)
     url = browser.current_url
     url = url.replace(url_body_1, '')
     url = url.replace(url_body_2, '')
@@ -72,22 +69,26 @@ def create_trips(From, To, Return, Start, Iterations):
     Dict_cities[To] = url.split('/')[1]
     
     browser.close()
-    # Second step : create list of trips by incrementing Timestamp
-    Days = [pd.Timestamp.now()+pd.Timedelta(days=Start+i) for i in range(Iterations)]
-    if Return==0:
-        Trips = [Dict_cities[From]+'/'+Dict_cities[To]+'/'+str(x)[2:4]+ \
-                 str(x)[5:7]+str(x)[8:10] for x in Days]
-    else: # Returning fligths
-        """
-        The stay length in days is shorten or broaden by 50% in the research
-        """
-        Trips = [Dict_cities[From]+'/'+Dict_cities[To]+'/'+str(x)[2:4]+ \
-                 str(x)[5:7]+str(x)[8:10] +"/"+ \
-                 str(x+pd.Timedelta(days=d + Return))[2:4] + \
-                 str(x+pd.Timedelta(days=d + Return))[5:7] + \
-                 str(x+pd.Timedelta(days=d + Return))[8:10] \
-                 for x in Days for d in range(-1 * Return//2, Return//2)]
-    return(Trips, Dict_cities)
+    
+    if len(Dict_cities[From])==0 or len(Dict_cities[To])==0: # Trip isn't supported
+        return([], False)
+    else:
+        # Second step : create list of trips by incrementing Timestamp
+        Days = [pd.Timestamp.now()+pd.Timedelta(days=Start+i) for i in range(Iterations)]
+        if Return==0:
+            Trips = [Dict_cities[From]+'/'+Dict_cities[To]+'/'+str(x)[2:4]+ \
+                     str(x)[5:7]+str(x)[8:10] for x in Days]
+        else: # Returning fligths
+            """
+            The stay length in days is shorten or broaden by 50% in the research
+            """
+            Trips = [Dict_cities[From]+'/'+Dict_cities[To]+'/'+str(x)[2:4]+ \
+                     str(x)[5:7]+str(x)[8:10] +"/"+ \
+                     str(x+pd.Timedelta(days=d + Return))[2:4] + \
+                     str(x+pd.Timedelta(days=d + Return))[5:7] + \
+                     str(x+pd.Timedelta(days=d + Return))[8:10] \
+                     for x in Days for d in range(-1 * Return//2, Return//2)]
+        return(Trips, True)
 
 # create_trips('Paris','Dubai',0,20,10)
 # create_trips('Paris','Dubai',10,20,5)
@@ -118,6 +119,17 @@ def price_alert_banner(browser):
         return 0
 
 def bot_detection(browser):
+    """ Coming soon   
+
+    try: # êtes-vous un robot?
+        detect = browser.find_element_by_xpath('//button[contains(text(), "Êtes-vous une personne ou un robot ?")]')
+        browser.close()
+        DF.to_excel("Skyscanner_PARIS.xlsx", index=False)
+        print('Bot detect !'), print(A)
+    except:
+        pass
+
+    """
     return(browser)
 
 def init_workbook(filename, Return):
@@ -133,10 +145,10 @@ def init_workbook(filename, Return):
     return 1
 
 def save_DF_to_workbook(filename, DF):
-    book = load_workbook(filename, index=False), 
+    book = load_workbook(filename) 
     writer = pd.ExcelWriter(filename, engine='openpyxl')
     writer.book=book #Save former sheets
-    DF.to_excel(writer, index=False)
+    DF.to_excel(writer, sheet_name='Sheet1', index=False)
     writer.save()
     return 1
 
@@ -146,9 +158,8 @@ def page_results_OneWay(browser, trip, DF, banners):
     Departure, Arrival and Date), appends results of web page to DataFrame
     Note that there are several banners to handle only once
     """
-    browser.get(url_body_1 + trip.split('_')[0]+"/"+ trip.split('_')[1] + \
-                "/" + trip.split('_')[2] + url_body_2)
-    WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.ID, "Layer_1")))
+    browser.get(url_body_1 + trip + url_body_2)
+    WebDriverWait(browser, 25).until(EC.presence_of_element_located((By.ID, "Layer_1")))
     
     # Cookie banner
     if not banners['cookie']:
@@ -187,16 +198,17 @@ def page_results_OneWay(browser, trip, DF, banners):
                             .find_element_by_tag_name("img").get_attribute("alt")
         except:
             comp = boxes[j].find_element_by_xpath('.//div[contains(@class, "TicketBody_legLogo_")]').text
-        DF.at[trip+'_'+str(j)] = [trip.split('_')[0], trip.split('_')[1], trip.split('_')[2],
+        DF.at[trip+'_'+str(j)] = [From, To,
+              trip.split('/')[2],
               boxes[j].find_element_by_xpath('.//span[contains(@class, "LegInfo_routePartialTime_")]').text, # Departure Time
               boxes[j].find_element_by_xpath('.//span[contains(@class, "Duration_duration_")]').text, # Flight Length
               direct_or_stops.split("\n")[0], # Direct ?
               comp, # Airtravel Company
-              price_currency.split(' ')[0], # Price
-              price_currency.split(' ')[1] # Currency
+              int(price_currency[:-2]), # Price
+              price_currency[-2:] # Currency
               ]
     del boxes
-    return 1
+    return(DF)
 
 def main_OneWay(Trips):
     """
@@ -211,8 +223,8 @@ def main_OneWay(Trips):
                           firefox_profile=webdriver.FirefoxProfile() \
                           .set_preference("browser.privatebrowsing.autostart", True))
     for j in range(len(Trips)):
-        page_results_OneWay(b, Trips[j], DF, banners)
-        if (j+1) % 5 == 0:
+        DF = page_results_OneWay(b, Trips[j], DF, banners)
+        if (j+1) % 12 == 0:
             save_DF_to_workbook(filename, DF)
             DF = pd.DataFrame(columns=['From','To','Date','Departure time',
                     'Duration','Direct flight','Price','Currency','Company'])
@@ -227,9 +239,8 @@ def page_results_Return(browser, trip, DF, banners):
     Departure, Arrival and Date), appends results of web page to DataFrame
     Note that there are several banners to handle only once
     """
-    browser.get(url_body_1 + trip.split('_')[0]+"/"+ trip.split('_')[1] + \
-                "/"+trip.split('_')[2] +"/"+ trip.split('_')[3] + url_body_2)
-    WebDriverWait(browser, 15).until(EC.presence_of_element_located((By.ID, "Layer_1")))
+    browser.get(url_body_1 + trip + url_body_2)
+    WebDriverWait(browser, 25).until(EC.presence_of_element_located((By.ID, "Layer_1")))
     
     # Cookie banner
     if not banners['cookie']:
@@ -277,22 +288,23 @@ def page_results_Return(browser, trip, DF, banners):
                               .find_element_by_tag_name("img").get_attribute("alt")
         except:
             comp_2 = ways[1].find_element_by_xpath('.//div[contains(@class, "TicketBody_legLogo_")]').text
-        DF.at[trip+'_'+str(j)] = [trip.split('_')[0], trip.split('_')[1], trip.split('_')[2],
+        DF.at[trip+'_'+str(j)] = [From, To,
+              trip.split('/')[2],
               ways[0].find_element_by_xpath('.//span[contains(@class, "LegInfo_routePartialTime_")]').text, # Departure Time
               ways[0].find_element_by_xpath('.//span[contains(@class, "Duration_duration_")]').text, # Flight Length
               direct_or_stops_1.split("\n")[0], # Direct ?
               comp_1, # Airtravel Company
-              trip.split('_')[3],
+              trip.split('/')[3],
               ways[1].find_element_by_xpath('.//span[contains(@class, "LegInfo_routePartialTime_")]').text, # Departure Time
               ways[1].find_element_by_xpath('.//span[contains(@class, "Duration_duration_")]').text, # Flight Length
               direct_or_stops_2.split("\n")[0], # Direct ?
               comp_2, # Airtravel Company
-              price_currency.split(' ')[0], # Price
-              price_currency.split(' ')[1] # Currency
+              int(price_currency[:-2]), # Price
+              price_currency[-2:] # Currency
               ]
         del ways
     del boxes
-    return 1
+    return(DF)
 
 
 def main_Return(Trips):
@@ -310,8 +322,8 @@ def main_Return(Trips):
                           firefox_profile=webdriver.FirefoxProfile() \
                           .set_preference("browser.privatebrowsing.autostart", True))
     for j in range(len(Trips)):
-        page_results_Return(b, Trips[j], DF, banners)
-        if (j+1) % 5 == 0:
+        DF = page_results_Return(b, Trips[j], DF, banners)
+        if (j+1) % 12 == 0:
             save_DF_to_workbook(filename, DF)
             DF = pd.DataFrame(columns=['From','To','Date - OneWay','Departure time - OneWay',
             'Duration - OneWay','Direct flight - OneWay','Company - OneWay',
@@ -326,39 +338,34 @@ def main_Return(Trips):
 # --- MAIN PROGRAM ---
 # =============================================================================
 
-# Workbook for results
-filename = "Workbooks/Skyscanner_from_" + From + "_To_" + To + "_" + \
-                ("One-Way.xlsx" if Return==0 else "Return.xlsx")
-init_workbook(filename, Return)
+
+# Our Departure and Arrival Cities / One-Way or Return flights
+From = input("Departure City : ")
+To = input("Destination City : ")
+Return = int( \
+         input("Estimated stay in number of days (0 for One-Way flight) : "))
+
 
 # Trips
-Trips, Dict_cities = create_trips(From, To, Return, Start, Iterations)
+Trips, exist = create_trips(From, To, Return, Start, Iterations)
 
-# Divide task in N browsers
-N = len(Trips)
-Inputs = []
-for i in range(N_browsers):
-    Inputs.append(Trips[i*(N//N_browsers):(i+1)*(N//N_browsers)])
-
-with mp.Pool(N_processes) as p:
-    if Return==0:
-        DF_list = p.map(main_OneWay, Inputs)
-    else:
-        DF_list = p.map(main_Return, Inputs)
-#
-print(DF_list)
-
-"""
-
-NEXT STEP : BOT PAGE DETECTION
-
-
-try: # êtes-vous un robot?
-    detect = browser.find_element_by_xpath('//button[contains(text(), "Êtes-vous une personne ou un robot ?")]')
-    browser.close()
-    DF.to_excel("Skyscanner_PARIS.xlsx", index=False)
-    print('Bot detect !'), print(A)
-except:
-    pass
-"""
+if not exist:
+    print("This trip isn't supported by the app")
+else:
+    # Workbook for results
+    filename = "Workbooks/Skyscanner_from_" + From + "_To_" + To + "_" + \
+                    ("One-Way.xlsx" if Return==0 else "Return.xlsx")
+    init_workbook(filename, Return)
+    # Divide task into N browsers
+    N = len(Trips)
+    Inputs = []
+    for i in range(N_browsers):
+        Inputs.append(Trips[i*(N//N_browsers):(i+1)*(N//N_browsers)])
+    
+    with mp.Pool(N_processes) as p:
+        if Return==0:
+            DF_list = p.map(main_OneWay, Inputs)
+        else:
+            DF_list = p.map(main_Return, Inputs)
+#    DF_list.to_excel("Finale-"+filename, index=False)
 ###
